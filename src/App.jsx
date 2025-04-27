1a89e5a43ce16d51608ac2ac1ac90a3c602f83d4
David Kalmus <dejvios@seznam.cz>
2025-04-27 23:01:54 +0200

    sneakers-app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
 import AllSneakers from "./pages/AllSneakers"
 import SneakerDetails from "./pages/SneakerDetails"
-import SharedLayout from "./components/SharedLayout"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <SharedLayout />,
-    children: [
-      { index: true, element: <AllSneakers /> },
-      { path: "/:sneakerId", element: <SneakerDetails /> }
-    ]
-  },
+  { path: "/", element: <AllSneakers /> },
+  { path: "/:sneakerId", element: <SneakerDetails /> }
 ])
 
 const App = () => {
